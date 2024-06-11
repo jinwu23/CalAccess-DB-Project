7f28a2a319bd94d969e39c8fb272c16e5dce888a
Jin Wu <69867265+jinwu23@users.noreply.github.com>
2024-06-11 01:53:58 -0700

    Update db_test.py

---
+++ db_test.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+from db_connect import db_connect
+
+load_dotenv()
+
+connection = db_connect(os.getenv('CALACCESSDB'))
+cursor = connection.cursor()
+
+# print all tables
+cursor.execute("SHOW TABLES")
+tables = cursor.fetchall()
+print("Table Count: ", cursor.rowcount)
+for table in tables:
+    print(table[0])
