f4ddf241f9790c8b7139cce23ffa3bf5090268d2
Jin Wu <69867265+jinwu23@users.noreply.github.com>
2024-06-11 01:54:31 -0700

    Create DB-cleanup.sql

---
+++ DB-cleanup.sql
@@ -0,0 +1,20 @@
+-- TEAM DATACRATS
+-- Jin Wu
+-- Last Updated: 6/10/2024
+
+-- Relationship Sets
+DROP TABLE IF EXISTS InfluenceBill;
+DROP TABLE IF EXISTS FirmHireLobbyist;
+DROP TABLE IF EXISTS OrgHireLobbyist;
+DROP TABLE IF EXISTS HireFirm;
+
+
+-- Entity Sets
+DROP TABLE IF EXISTS Bills;
+
+DROP TABLE IF EXISTS LobbyingFirms;
+DROP TABLE IF EXISTS Organizations;
+
+DROP TABLE IF EXISTS Lobbyists;
+DROP TABLE IF EXISTS Persons;
+DROP TABLE IF EXISTS Filer;
